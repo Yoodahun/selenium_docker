f8356b8d64e93e9983524aeabd796c9158f5e3ec
Yoodahun <dahun4032@gmail.com>
2022-03-10 00:11:24 +0900

    Add createDevTools method

--- src/test/java/driverFactory/DriverFactory.java
+++ src/test/java/driverFactory/DriverFactory.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.devtools.DevTools;
 
 import java.time.Duration;
 
@@ -25,6 +26,11 @@ public class DriverFactory {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         return driver;
+    }
+
+    public static DevTools createDevTools() {
+
+        return  driver.getDevTools();
     }
 
     public static void closeDriver() {
